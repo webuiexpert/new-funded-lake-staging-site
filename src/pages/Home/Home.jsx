4584b69dc584be45220aca6ca535938fd339fdfd
webuiexpert <webuiexpert929@gmail.com>
2025-10-21 19:04:47 +0530

    Add meta tags

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,10 +11,51 @@ import CertificateMarqueeSlider from './components/CertificateMarqueeSlider'
 import TraderWorld from './components/TraderWorld'
 import SuccessStory from './components/SuccessStory'
 import TrustPilotMarquee from './components/TrustPilotMarquee'
+import { Helmet } from "react-helmet";
+
 
 function Home() {
   return (
     <div className='overflow-hidden'>
+      <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="profile" href="http://gmpg.org/xfn/11" />
+          <link rel="pingback" href="https://fundedlake.com/xmlrpc.php" />
+          <meta
+            name="robots"
+            content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+          />
+          <title>FUNDEDLAKE – Where traders empower traders</title>
+          <meta
+            name="description"
+            content="FUNDEDLAKE offers challenges up to $400k, instant funding up to $100k(no consistency rule), fast payouts, and instant support!"
+          />
+          <link rel="canonical" href="https://fundedlake.com/" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="FUNDEDLAKE – Where traders empower traders"
+          />
+          <meta
+            property="og:description"
+            content="FUNDEDLAKE offers challenges up to $400k, instant funding up to $100k(no consistency rule), fast payouts, and instant support!"
+          />
+          <meta property="og:url" content="https://fundedlake.com/" />
+          <meta property="og:site_name" content="FundedLake" />
+          <meta
+            property="article:modified_time"
+            content="2025-05-03T23:18:48+00:00"
+          />
+          <meta
+            property="og:image"
+            content="https://fundedlake.com/wp-content/uploads/2025/04/cropped-android-chrome-512x512-2.png"
+          />
+          <meta property="og:image:width" content="512" />
+          <meta property="og:image:height" content="512" />
+          <meta property="og:image:type" content="image/png" />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Helmet>
       <HeroSection />
       <ChooseUs />
       <HowWorks />
--- src/pages/Who we are/WhoWeAre.jsx
+++ src/pages/Who we are/WhoWeAre.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import WhoWeSection from './components/WhoWeSection'
-import { div } from 'framer-motion/client'
+import { Helmet } from "react-helmet";
 
 function WhoWeAre() {
   return (
     <div className=''>
+        <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="pingback" href="https://fundedlake.com/xmlrpc.php" />
+        <meta
+          name="robots"
+          content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+        />
+        <title>Who We Are - FundedLake</title>
+        <meta
+          name="description"
+          content="FundedLake is a prop trading firm offering instant funding up to $400K, fast payouts, and flexible rules for traders of all levels."
+        />
+        <link rel="canonical" href="https://fundedlake.com/who-we-are/" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Who We Are - FundedLake" />
+        <meta
+          property="og:description"
+          content="FundedLake is a prop trading firm offering instant funding up to $400K, fast payouts, and flexible rules for traders of all levels."
+        />
+        <meta property="og:url" content="https://fundedlake.com/who-we-are/" />
+        <meta property="og:site_name" content="FundedLake" />
+        <meta
+          property="article:modified_time"
+          content="2025-04-11T06:43:34+00:00"
+        />
+        <meta
+          property="og:image"
+          content="https://fundedlake.com/wp-content/uploads/2025/04/favicon.png"
+        />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+        <meta property="og:image:type" content="image/png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:label1" content="Est. reading time" />
+        <meta name="twitter:data1" content="1 minute" />
+      </Helmet>
       <WhoWeSection />
     </div>
     )
